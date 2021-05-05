f59b545b67329e94b21fc22777094917544431c4
cmerin0 <carlosmerino@gmail.com>
2021-05-05 15:49:20 -0600

    Agregando guia 6 + funcionamiento

---
+++ Guia#6/EjerciciosPropuestos/app/src/main/java/com/udb/dsm/layoutapp/MainActivity.java
@@ -0,0 +1,21 @@
+package com.udb.dsm.layoutapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ejercicio_2);
+//        setContentView(R.layout.ejercicio_1);
+//        setContentView(R.layout.ejemplo_grid_layout);
+//        setContentView(R.layout.ejemplo_table_layout);
+//        setContentView(R.layout.ejemplo_relative_layout);
+//        setContentView(R.layout.ejemplo_linear_layout);
+//        setContentView(R.layout.ejemplo_frame_layout);
+//        setContentView(R.layout.activity_main);
+    }
+}
